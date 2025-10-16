364321ad3bd8b22655932a4140d049b8587b0fd1
Sangam Giri <girisangam00@gmail.com>
2025-10-16 10:05:22 +0545

    feat: multi level scroll

--- lib/main.dart
+++ lib/main.dart
@@ -15,28 +15,233 @@ class App extends StatelessWidget {
         body: CustomScrollView(
           slivers: [
             SliverAppBar(
-              expandedHeight: 300,
-
-              /// We can set minimum height for the appbar using this parameter
               toolbarHeight: 100,
-              backgroundColor: Colors.black,
-              pinned: true,
-
-              /// Using floating we can make our app bar visible regardless of where we scroll
-              // floating: true,
+              backgroundColor: Colors.white,
               flexibleSpace: FlexibleSpaceBar(
-                background: Image.network(
-                  'https://fujiframe.com/assets/images/_3000x2000_fit_center-center_85_none/10085/xhs2-fuji-70-300-Amazilia-Hummingbird.webp',
-                  fit: BoxFit.cover,
-                ),
-                title: Text(
-                  'Some Title',
-                  style: TextStyle(color: Colors.white),
+                background: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'Wlecome Back!',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                              SizedBox(height: 4),
+                              Text(
+                                'John Doe',
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black87,
+                                ),
+                              ),
+                            ],
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.notifications_outlined,
+                              color: Colors.deepPurple,
+                              size: 28,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-            SliverFillRemaining(),
+            SliverAppBar(
+              backgroundColor: Colors.deepPurple,
+              expandedHeight: 200,
+
+              /// This helps us view our balance by scrolling on anywhere without needing to scroll from top
+              floating: true,
+              flexibleSpace: FlexibleSpaceBar(
+                titlePadding: EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 10,
+                ),
+                background: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '\$5, 270',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Current Balance',
+                      style: TextStyle(color: Colors.white70, fontSize: 16),
+                    ),
+                  ],
+                ),
+                title: Text('Wallet', style: TextStyle(color: Colors.white)),
+              ),
+            ),
+            SliverAppBar(
+              backgroundColor: Colors.white,
+              expandedHeight: 170,
+              toolbarHeight: 170,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Quick actions',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        _buildQuickAction(icon: Icons.send, label: 'Send'),
+                        _buildQuickAction(
+                          icon: Icons.receipt,
+                          label: 'Request',
+                        ),
+                        _buildQuickAction(icon: Icons.add, label: 'Top Up'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            /// We cannot directly use regular widget inside of Sliver and thus we make use of SliverToBoxAdapter
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Text(
+                  'Reent Transactions',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return _buildTransactionTile(
+                  title: 'Transaction $index',
+                  subtitle: 'Details of transaction $index',
+                  amount: '${index.isEven ? '+' : '-'}${index + 1 * 20}',
+                  isPositive: index.isEven,
+                );
+              }, childCount: 20),
+            ),
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.lightbulb_outline,
+                    size: 64,
+                    color: Colors.deepPurple,
+                  ),
+
+                  SizedBox(height: 16),
+                  Text(
+                    'Explore New Features',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Stay tuned for exciting updated and important information',
+                    style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 24),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 16,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                      ),
+                    ),
+                    child: Text(
+                      'Lean more',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                ],
+              ),
+            ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildQuickAction({required IconData icon, required String label}) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 30,
+          backgroundColor: Colors.deepPurple.withOpacity(0.1),
+          child: Icon(icon, color: Colors.deepPurple, size: 30),
+        ),
+        SizedBox(height: 8),
+        Text(label, style: TextStyle(fontSize: 14)),
+      ],
+    );
+  }
+
+  Widget _buildTransactionTile({
+    required String title,
+    required String subtitle,
+    required String amount,
+    required bool isPositive,
+  }) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.deepPurple.withOpacity(0.1),
+            child: Icon(
+              isPositive ? Icons.arrow_upward : Icons.arrow_downward,
+              color: isPositive ? Colors.green : Colors.red,
+            ),
+          ),
+          title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
+          subtitle: Text(subtitle),
+          trailing: Text(
+            amount,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: isPositive ? Colors.green : Colors.red,
+            ),
+          ),
         ),
       ),
     );
